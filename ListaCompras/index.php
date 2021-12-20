77d0bd0d8a8a31c1a7c5ca10490ea6287b0d2b79
Caiovlima2 <43886910+Caiovlima2@users.noreply.github.com>
2021-12-20 12:47:11 -0300

    Add files via upload

--- ListaCompras/index.php
+++ ListaCompras/index.php
@@ -9,6 +9,20 @@
     <script src=js/bootstrap.bundle.min.js></script>
 </head>
 <body>
+
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Login</label>
+    <input type="text" class="form-control" id="login_usuario">
+    <div id="emailHelp" class="form-text">Nunca compartilhe seu login com ninguém!</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Senha</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+
+  <button type="submit" class="btn btn-primary" style="width: 100%;">Submit</button>
+</form>
   
 </body>
 </html>
